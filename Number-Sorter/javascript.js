25fc3096e39d37c71c19f05a0626c375ee7d558e
Mike0895 <michael.morocho05@gmail.com>
2024-04-24 19:51:55 +0200

    función con loop

--- Number-Sorter/javascript.js
+++ Number-Sorter/javascript.js
@@ -6,13 +6,21 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
+
+  updateUI(inputValues);
 }
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
-
+    outputValueNode.innerText = num;
   })
+}
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+
+  }
 }
 
 sortButton.addEventListener("click", sortInputArray);
